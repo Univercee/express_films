2562041387b62fd71d66b5b8c423370bf9ce02e0
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-07-04 20:13:13 +0300

    init compression

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet')
+var compression = require('compression')
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression())
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
