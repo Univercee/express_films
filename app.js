bb276c9edcadcde727ce58fb2dcd72230b3818a1
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-07-04 20:08:11 +0300

    init helmet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet')
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
@@ -20,6 +21,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "default-src": ["'self'"],
+      "script-src": ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"],
+      "connect-src": ["https://moviesdatabase.p.rapidapi.com"],
+      "img-src": ["*"]
+    },
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
