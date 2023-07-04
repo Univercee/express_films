ad41a28c5d612810591e40262ef387ac139933c7
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-07-04 16:12:56 +0300

    use ejs engine

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cons = require('consolidate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.engine('html', cons.swig)
+app.engine('.html', require('ejs').__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
