968e782cb0c4a046504ad6fbfb6c40651731a379
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-07-09 15:56:56 +0300

    add google analytics to security policy

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       "default-src": ["'self'"],
-      "script-src": ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"],
-      "connect-src": ["'self'", "https://moviesdatabase.p.rapidapi.com"],
+      "script-src": ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js", "https://www.googletagmanager.com"],
+      "connect-src": ["'self'", "https://moviesdatabase.p.rapidapi.com", "https://www.google-analytics.com"],
       "img-src": ["*"]
     },
   })
